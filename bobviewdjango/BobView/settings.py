3b106591e119a5c20cdb83189bdeb1736dc79f85
Wonhee Cho <42177510+jowoni@users.noreply.github.com>
2019-10-29 16:48:11 +0900

    Update settings.py

--- bobviewdjango/BobView/settings.py
+++ bobviewdjango/BobView/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5@%y4z64ma6ez5=ove-v2jyixheqo+qvgx4^%vfnvubm8bhq@m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
