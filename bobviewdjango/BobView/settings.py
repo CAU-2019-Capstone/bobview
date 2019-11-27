b613d2c1aa1651f30e40bbc950cd6d24a7927186
canis617 <whwhdgus96@gmail.com>
2019-11-27 19:08:54 +0900

    db link error

--- bobviewdjango/BobView/settings.py
+++ bobviewdjango/BobView/settings.py
@@ -15,7 +15,7 @@ import configparser
 
 config = configparser.ConfigParser()
 #TODO
-config.read('/home/hyuncho/workspace/database/config.ini')
+config.read('/home/ubuntu/database/config.ini')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
