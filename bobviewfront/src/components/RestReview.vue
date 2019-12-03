3dacb232637a82ebe6c5109d5e3d3c494eb5ff7e
canis617 <whwhdgus96@gmail.com>
2019-12-03 20:10:52 +0900

    color changed9

--- bobviewfront/src/components/RestReview.vue
+++ bobviewfront/src/components/RestReview.vue
@@ -26,7 +26,7 @@
         <div class="justify-center" v-if="rest_rating['restaurant']['restaurant_image']==''">No Image</div>
         <v-divider></v-divider>
         <div class="px-4 py-2">
-            <h5>{{menu_rating['user']['username']}} 's Review</h5>
+            <h5>{{rest_rating['user']['username']}} 's Review</h5>
             <h4 class="mb-0">{{rest_rating['desc']}}</h4>
         </div>
         <v-card-actions>
